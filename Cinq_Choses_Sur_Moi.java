d323587168c05919220a7275c3d67202aa2595ba
GuillaumeCornette <48519522+GuillaumeCornette@users.noreply.github.com>
2020-07-28 11:21:02 +0200

    Create Cinq_Choses_Sur_Moi.java

---
+++ Cinq_Choses_Sur_Moi.java
@@ -0,0 +1,23 @@
+public void Cinq_Choses_Sur_Moi(){
+        //1
+        while(true){
+            System.out.print("Je suis toujours motivé");
+            break;
+        }
+        //2
+        if (problem){
+            System.out.println("Je suis patient et aime résoudre des problèmes");
+        }
+        //3
+        if (needInformation){
+           System.out.println("Je suis étudiant en Sciences Informatique, je serai en master en Cybersécurité en Septembre");
+        }
+        //4
+        if (raisonJob){
+            System.out.println("J'aime enseigner, je donne cours à des seniors sur les bases de l'informatique");
+        }
+        //5
+        else {
+            System.out.println("Je sais applaudir à une main");
+        }
+    }
